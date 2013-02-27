4e5f5149537651258e158d43dd1eda69cea8d033
Kostas Antonopoulos <kostas@devpro.gr>
2013-02-27 04:17:11 +0200

    Initial Commit

---
+++ DPGoogleMapsAPIPlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DPGoogleMapsAPIPlaces
+//
+//  Created by Kostas Antonopoulos on 27/2/13.
+//  Copyright (c) 2013 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DPGoogleMapsAPIPlaces/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DPGoogleMapsAPIPlaces
+//
+//  Created by Kostas Antonopoulos on 27/2/13.
+//  Copyright (c) 2013 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
